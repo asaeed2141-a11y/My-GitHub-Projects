68de6a95b1fce2a2b7270e01318e19586c6ac828
Ali Saeed <asaeed2141@gmail.com>
2026-09-08 17:35:20 +0100

    upgrade password strength checker

--- Password-Strength-Checker/password_checker.py
+++ Password-Strength-Checker/password_checker.py
@@ -1,24 +1,46 @@
-password = input("Enter your password: ")
+def check_password(password):
+    score = 0
+    suggestions = []
 
-score = 0
-special_characters = "!@#$%^&*"
+    special_characters = "!@#$%^&*"
 
-if len(password) >= 8:
-    score += 1
+    if len(password) >= 8:
+        score += 1
+    else:
+        suggestions.append("Use at least 8 characters")
 
-if any(char.isupper() for char in password):
-    score += 1
+    if any(char.isupper() for char in password):
+        score += 1
+    else:
+        suggestions.append("Add an uppercase letter")
 
-if any(char.islower() for char in password):
-    score += 1
+    if any(char.islower() for char in password):
+        score += 1
+    else:
+        suggestions.append("Add a lowercase letter")
 
-if any(char.isdigit() for char in password):
-    score += 1
+    if any(char.isdigit() for char in password):
+        score += 1
+    else:
+        suggestions.append("Add a number")
 
-if any(char in special_characters for char in password):
-    score += 1
+    if any(char in special_characters for char in password):
+        score += 1
+    else:
+        suggestions.append("Add a special character")
 
-print("\nPassword Score:", score, "/ 5")
+    return score, suggestions
+
+
+print("================================")
+print("   PASSWORD STRENGTH CHECKER")
+print("================================")
+
+password = input("\nEnter your password: ")
+
+score, suggestions = check_password(password)
+
+print("\nScore:", score, "/ 5")
 
 if score <= 2:
     print("Strength: WEAK")
@@ -26,3 +48,12 @@ elif score <= 4:
     print("Strength: MEDIUM")
 else:
     print("Strength: STRONG")
+
+if suggestions:
+    print("\nSuggestions:")
+
+    for suggestion in suggestions:
+        print("-", suggestion)
+else:
+    print("\nExcellent! Your password meets all requirements.")
+
